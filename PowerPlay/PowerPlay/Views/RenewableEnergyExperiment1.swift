d6675d1978497f2d10b8146767e66f625a6086cc
Nishka Sharma <nishkasharma@Nishkas-MacBook-Air.local>
2023-07-14 19:03:40 -0700

    discover page added

---
+++ PowerPlay/PowerPlay/Views/RenewableEnergyExperiment1.swift
@@ -0,0 +1,20 @@
+//
+//  RenewableEnergyExperiment1.swift
+//  PowerPlay
+//
+//  Created by Nishka Sharma on 7/14/23.
+//
+
+import SwiftUI
+
+struct RenewableEnergyExperiment1: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct RenewableEnergyExperiment1_Previews: PreviewProvider {
+    static var previews: some View {
+        RenewableEnergyExperiment1()
+    }
+}
