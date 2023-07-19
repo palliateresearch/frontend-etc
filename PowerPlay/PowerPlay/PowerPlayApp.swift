9ae308c98c08c7d07d4351d2be23405f321b92d7
Aadit Noronha <aadit.noronha@gmail.com>
2023-07-19 14:55:59 -0700

    Commit

--- PowerPlay/PowerPlay/PowerPlayApp.swift
+++ PowerPlay/PowerPlay/PowerPlayApp.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 @main
 struct PowerPlayApp: App {
-    
+    @ObservedObject var userData = UserViewData()
     @State var park = ""
     var body: some Scene {
         WindowGroup {
-            StartView()
+            EnterChildren(userData: userData)
         }
     }
 }
--- PowerPlay/PowerPlay/Views/Parent/EnterChildren.swift
+++ PowerPlay/PowerPlay/Views/Parent/EnterChildren.swift
@@ -1,32 +1,24 @@
 import SwiftUI
 
 struct EnterChildren: View {
-    
-    @ObservedObject var userData = UserViewData()
-    
-    @State private var lenChildren: Int = 0
-    @State private var current: Int = 0
-    @State private var isSubmitted = false
+    @ObservedObject var userData: UserViewData
     @State private var isPark = false
-    
+
     var body: some View {
         GeometryReader { geometry in
             let width = geometry.size.width
             let height = geometry.size.height
             VStack(spacing: 0) {
-                // Add Children Header
                 Text("Add Children")
                     .font(.title)
                     .fontWeight(.bold)
                     .padding()
                     .frame(maxWidth: .infinity)
                     .foregroundColor(Color.white)
-                
+
                 HStack {
                     Spacer()
                     Button(action: {
-                        // Add a child
-                        lenChildren += 1
                         userData.children.append("")
                     }) {
                         Image(systemName: "plus.circle")
@@ -34,37 +26,43 @@ struct EnterChildren: View {
                             .foregroundColor(Color("lightningYellow"))
                     }
                     .padding(.trailing, width * 0.05)
-                    .padding(.top, 16)
+                    .padding(.top, height * 0.1)
+                    .padding(.bottom, height * 0.1)
                 }
-                
+
                 ScrollView {
-                    VStack() {
-                        ForEach(0..<lenChildren, id: \.self) { num in
-                            ChildView(removeAction: {
-                                // Remove the child
-                                userData.children.remove(at: num)
-                                lenChildren -= 1
-                                
-                            }, text: Binding(
-                                get: {
-                                    userData.children.indices.contains(num) ? userData.children[num] : ""
-                                },
-                                set: {
-                                    if userData.children.indices.contains(num) {
-                                        userData.children[num] = $0
-                                    } else {
-                                        userData.children.append($0)
-                                    }
-                                }
-                            )).padding(.top, geometry.size.height * 0.1)
-                        }
-                    }
-                    .frame(maxWidth: .infinity)
+                   VStack(spacing: 0) { // Wrap the VStack with a divider
+                       ForEach(userData.children.indices, id: \.self) { index in
+                           ChildView(removeAction: {
+                               userData.children.remove(at: index)
+                           }, child: Binding(
+                               get: {
+                                   userData.children.indices.contains(index) ? userData.children[index] : ""
+                               },
+                               set: {
+                                   if userData.children.indices.contains(index) {
+                                       userData.children[index] = $0
+                                   } else {
+                                       userData.children.append($0)
+                                   }
+                               }
+                           ))
+                           .padding(.top, geometry.size.height * 0.05)
+
+                           if index != userData.children.count - 1 { // Add divider unless it's the last child
+                               Divider()
+                                   .background(Color.gray)
+                                   .padding(.horizontal, geometry.size.width * 0.05)
+                                   .padding(.top, height * 0.075)
+                           }
+                       }
+                   }
+                   .frame(maxWidth: .infinity)
                 }
-                
+
                 Button(action: {
-                   isPark = true
-                }){
+                    isPark = true
+                }) {
                     Text("Submit")
                         .font(.system(size: width * 0.06, weight: .bold))
                         .frame(width: width * 0.5, height: height * 0.075)
@@ -72,11 +70,10 @@ struct EnterChildren: View {
                         .foregroundColor(Color.black)
                         .opacity(0.8)
                         .cornerRadius(10)
-                }.padding(.top, height * 0.06)
-                
+                }
+                .padding(.top, height * 0.04)
             }
             .preferredColorScheme(.dark)
-            
         }
         .fullScreenCover(isPresented: $isPark) {
             FindPark(userData: userData)
@@ -84,52 +81,21 @@ struct EnterChildren: View {
     }
 }
 
-
 struct ChildView: View {
     var removeAction: () -> Void
-    @Binding var text: String
-    @State private var isEditing: Bool = false
+    @Binding var child: String
 
     var body: some View {
         GeometryReader { geometry in
             let width = geometry.size.width
             let height = geometry.size.height
-            let boxWidth = width * 0.8
-            let boxHeight = height * 8
             let spacing = width * 0.01
-          
+
             HStack {
                 ZStack {
-                    RoundedRectangle(cornerRadius: 12)
-                        .foregroundColor(Color("navyBlue"))
-                        .frame(width: boxWidth, height: boxHeight)
-                        .onTapGesture {
-                            isEditing.toggle() // Toggle the isEditing state on tap
-                        }
-
-                    if isEditing {
-                        TextField("", text: $text, onEditingChanged: { editing in
-                            // This will be called when the editing begins and ends
-                            if !editing {
-                                isEditing = false // Set isEditing to false when editing is finished
-                            }
-                        })
-                        .font(.system(size: width * 0.05, weight: .bold))
-                        .padding(width * 0.03)
-                        .foregroundColor(Color.white)
-                    } else {
-                        if text.isEmpty {
-                            Text("Enter child's username")
-                                .foregroundColor(Color.gray)
-                                .font(.system(size: width * 0.05, weight: .bold))
-                                .padding(width * 0.03)
-                        } else {
-                            Text(text)
-                                .foregroundColor(Color.gray)
-                                .font(.system(size: width * 0.05, weight: .bold))
-                                .padding(width * 0.03)
-                        }
-                    }
+                   
+                    TextField("Child Username", text: self.$child)
+                        .padding(.horizontal, width * 0.05)
                 }
 
                 Spacer()
@@ -143,9 +109,14 @@ struct ChildView: View {
                 }
                 .padding(.trailing, width * 0.045)
                 .padding(.leading, width * 0.05)
-                // Add padding between the minus sign and the box
             }
-            .padding(.horizontal, spacing) // Add horizontal padding with the same spacing
+            .padding(.horizontal, spacing)
         }
+    }
+}
+
+struct EnterChildren_Previews: PreviewProvider {
+    static var previews: some View {
+        EnterChildren(userData: UserViewData()).environment(\.colorScheme, .dark)
     }
 }
