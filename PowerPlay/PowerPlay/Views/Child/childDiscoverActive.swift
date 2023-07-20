9807de797a6e85a36ed05ccd54bbab261282d32d
Nishka Sharma <nishkasharma@Nishkas-MacBook-Air.local>
2023-07-20 16:36:55 -0700

    scroll for discover working

---
+++ PowerPlay/PowerPlay/Views/Child/childDiscoverActive.swift
@@ -0,0 +1,42 @@
+//
+//  childDiscoverActive.swift
+//  PowerPlay
+//
+//  Created by Nishka Sharma on 7/20/23.
+//
+
+import SwiftUI
+
+struct childDiscoverActive: View {
+    
+    var body: some View {
+        NavigationStack{
+            
+            VStack{
+                childCustomNavBar(navTitle: "Discover", color: "darkBlue")
+                Spacer()
+                Image("sparkyWithClouds")
+                    .scaleEffect(2)
+                    .padding()
+                    .padding()
+                    .padding()
+                Spacer()
+                childStartLesson(progress: 0.333333, background: "darkBlue", text: "aliceBlue", progressDone: "lightningYellow", shadow: "dropShadowBlue")
+                Spacer()
+              
+            }
+            .background(Color("lightBlue"))
+
+        }
+        .background(Color("lightBlue"))
+    }
+}
+
+struct childDiscoverActive_Previews: PreviewProvider {
+    static var previews: some View {
+        childDiscoverActive()
+    }
+}
+
+
+
