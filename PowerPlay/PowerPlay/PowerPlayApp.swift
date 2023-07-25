62c51bb018780fb1bb5adfd3b084b85e9286b310
Aadit Noronha <aadit.noronha@gmail.com>
2023-07-25 14:03:28 -0700

    amp

--- PowerPlay/PowerPlay/PowerPlayApp.swift
+++ PowerPlay/PowerPlay/PowerPlayApp.swift
@@ -3,8 +3,6 @@ import ConfettiSwiftUI
 
 @main
 struct PowerPlayApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    @StateObject private var monitoringManager = monitorStreak()
     @StateObject private var pv = PV()
     var model = TestModel()
     var body: some Scene {
@@ -23,11 +21,3 @@ struct PowerPlayApp: App {
     }
 }
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        // Initialize the monitoring manager here, so it starts as soon as the app launches
-        let monitoringManager = monitorStreak()
-
-        return true
-    }
-}
